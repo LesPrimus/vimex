11045e454c27f0174991be6329e5efa38c296783
LesPrimus <amerigo.armentano@hotmail.it>
2023-01-11 13:17:24 +0100

    Add device code auth flow test raise.

--- tests/test_auth/test_device_code.py
+++ tests/test_auth/test_device_code.py
@@ -48,3 +48,33 @@ class TestDeviceCodeAuth:
         request = flow.send(response)
 
         assert request.headers["Authorization"] == f"Bearer some_access_token"
+
+    @mock.patch("vimex._auth.VimeoOauth2DeviceCodeGrant.poll_authorize_url")
+    def test_auth_flow_with_grant_raise_time_out_error(self, mock_poll_authorize_url):
+        mock_poll_authorize_url.side_effect = TimeoutError()
+
+        auth = vimex.VimeoOauth2DeviceCodeGrant(
+            CLIENT_ID, CLIENT_SECRET, state="long___state"
+        )
+        request = httpx.Request("GET", API_ROOT)
+
+        flow = auth.auth_flow(request)
+
+        request = next(flow)
+
+        assert request.headers["Authorization"].startswith("basic")
+
+        json_response = vimex.DeviceCodeGrantResponse(
+            device_code="some_device_code",
+            user_code="some_user_code",
+            authorize_link="https://some-authorize-link",
+            activate_link="https://some-activate-link",
+            expires_in=600,
+            interval=10,
+        )._asdict()
+
+        response = httpx.Response(status_code=200, json=json_response)
+
+        with pytest.raises(TimeoutError):
+            request = flow.send(response)
+            assert request.headers["Authorization"] == f"Bearer some_access_token"
