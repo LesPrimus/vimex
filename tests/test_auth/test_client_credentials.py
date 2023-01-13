d960ab7b3268a0ff21de862d3121c8e70dde5cc4
LesPrimus <amerigo.armentano@hotmail.it>
2023-01-13 17:38:07 +0100

    Update client credentials tests.

--- tests/test_auth/test_client_credentials.py
+++ tests/test_auth/test_client_credentials.py
@@ -1,48 +1,76 @@
 import httpx
+import pytest
 
 import vimex
 
 CLIENT_ID = "some_long_id"
 CLIENT_SECRET = "some_very_secret"
+STATE = "vERYlONGsTate"
 API_ROOT = "https://some_website.com"
 
 
 class TestClientCredentialsAuth:
-    def test_auth_flow_set_header_with_200(self):
+    def test_client_credentials_flow_with_200(self):
         auth = vimex.VimeoOAuth2ClientCredentials(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
         )
         request = httpx.Request("GET", API_ROOT)
-
-        flow = auth.auth_flow(request)
-
+        flow = auth.sync_auth_flow(request)
         request = next(flow)
         assert request.headers["Authorization"].startswith("basic")
-
         json_response = {
             "access_token": "some_access_token",
             "token_type": "bearer",
-            "scope": "foo, bar"
+            "scope": STATE,
         }
-
         response = httpx.Response(status_code=200, json=json_response)
         request = flow.send(response)
-        assert request.headers['Authorization'] == f"Bearer {json_response['access_token']}"
+        assert request.headers[auth.header_name] == auth.header_value.format(
+            token=json_response[auth.token_field_name]
+        )
 
-    def test_auth_flow_set_header_with_400(self):
+    def test_client_credentials_flow_with_400(self):
         auth = vimex.VimeoOAuth2ClientCredentials(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
         )
         request = httpx.Request("GET", API_ROOT)
-
-        flow = auth.auth_flow(request)
-
+        flow = auth.sync_auth_flow(request)
         request = next(flow)
         assert request.headers["Authorization"].startswith("basic")
-
         response = httpx.Response(status_code=400)
         request = flow.send(response)
+        assert "Authorization" not in request.headers
 
+
+@pytest.mark.anyio
+class TestAsyncClientCredentialsAuth:
+    async def test_async_client_credentials_flow_with_200(self):
+        auth = vimex.VimeoOAuth2ClientCredentials(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
+        )
+        request = httpx.Request("GET", API_ROOT)
+        flow = auth.async_auth_flow(request)
+        request = await anext(flow)
+        assert request.headers["Authorization"].startswith("basic")
+        json_response = {
+            "access_token": "some_access_token",
+            "token_type": "bearer",
+            "scope": STATE,
+        }
+        response = httpx.Response(status_code=200, json=json_response)
+        request = await flow.asend(response)
+        assert request.headers[auth.header_name] == auth.header_value.format(
+            token=json_response[auth.token_field_name]
+        )
+
+    async def test_client_credentials_flow_with_400(self):
+        auth = vimex.VimeoOAuth2ClientCredentials(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
+        )
+        request = httpx.Request("GET", API_ROOT)
+        flow = auth.async_auth_flow(request)
+        request = await anext(flow)
+        assert request.headers["Authorization"].startswith("basic")
+        response = httpx.Response(status_code=400)
+        request = await flow.asend(response)
         assert "Authorization" not in request.headers
