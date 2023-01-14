c453dd97b278dbef20cab35dcad815c2df01c7e9
LesPrimus <amerigo.armentano@hotmail.it>
2023-01-14 04:27:25 +0100

    Update implicit flow tests.

--- tests/test_auth/test_implicit_grant.py
+++ tests/test_auth/test_implicit_grant.py
@@ -1,45 +1,79 @@
-from unittest import mock
-
 import httpx
+import pytest
 
 import vimex
+from vimex._oauth2_server import MockedCallBackServer
 
 CLIENT_ID = "some_long_id"
 CLIENT_SECRET = "some_very_secret"
 API_ROOT = "https://some_website.com"
+STATE = "VeryLongState"
 
 
 class TestImplicitGrantAuth:
-    @mock.patch("vimex._auth.VimeoOauth2ImplicitGrant.get_authorization_grant")
-    def test_auth_flow_set_header_with_200(self, mocked_get_authorization_grant):
-        access_token = "some_access_token"
-        mocked_get_authorization_grant.return_value = (access_token, "SomeVeryLongState")
+    def test_implicit_flow_with_200(self):
         auth = vimex.VimeoOauth2ImplicitGrant(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            state="long___state"
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
         )
+        mocked_server = MockedCallBackServer()
+        auth.server = mocked_server
+
         request = httpx.Request("GET", API_ROOT)
+        flow = auth.sync_auth_flow(request)
 
-        flow = auth.auth_flow(request)
-
-        mocked_get_authorization_grant.assert_not_called()
+        mocked_server.result.access_token = "Some_access_token"
         request = next(flow)
-        mocked_get_authorization_grant.assert_called_once()
 
-        assert request.headers["Authorization"] == f"Bearer {access_token}"
-
-    @mock.patch("vimex._auth.VimeoOauth2ImplicitGrant.get_authorization_grant")
-    def test_auth_flow_dont_set_header_with_4xx(self, mocked_get_authorization_grant):
-        mocked_get_authorization_grant.return_value = None
-        auth = vimex.VimeoOauth2ImplicitGrant(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            state="long___state"
+        assert (
+            request.headers["Authorization"]
+            == f"Bearer {mocked_server.result.access_token}"
         )
-        request = httpx.Request("GET", API_ROOT)
 
-        flow = auth.auth_flow(request)
+    def test_implicit_flow_with_400(self):
+        auth = vimex.VimeoOauth2ImplicitGrant(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
+        )
+        mocked_server = MockedCallBackServer()
+        auth.server = mocked_server
+
+        request = httpx.Request("GET", API_ROOT)
+        flow = auth.sync_auth_flow(request)
 
         request = next(flow)
+
+        assert "Authorization" not in request.headers
+
+
+@pytest.mark.anyio
+class TestAsyncImplicitGrantAuth:
+    async def test_implicit_flow_with_200(self):
+        auth = vimex.VimeoOauth2ImplicitGrant(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
+        )
+        mocked_server = MockedCallBackServer()
+        auth.server = mocked_server
+
+        request = httpx.Request("GET", API_ROOT)
+        flow = auth.async_auth_flow(request)
+
+        mocked_server.result.access_token = "Some_access_token"
+        request = await anext(flow)
+
+        assert (
+            request.headers["Authorization"]
+            == f"Bearer {mocked_server.result.access_token}"
+        )
+
+    async def test_implicit_flow_with_400(self):
+        auth = vimex.VimeoOauth2ImplicitGrant(
+            client_id=CLIENT_ID, client_secret=CLIENT_SECRET, state=STATE
+        )
+        mocked_server = MockedCallBackServer()
+        auth.server = mocked_server
+
+        request = httpx.Request("GET", API_ROOT)
+        flow = auth.async_auth_flow(request)
+
+        request = await anext(flow)
+
         assert "Authorization" not in request.headers
